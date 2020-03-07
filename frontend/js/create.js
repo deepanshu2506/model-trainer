4514376685779e25e29a193a55b102e0f5e7c9e0
shubh <shahshubh1010@gmail.com>
2020-03-07 23:21:53 +0530

    create model convert to array

--- frontend/js/create.js
+++ frontend/js/create.js
@@ -38,11 +38,46 @@ $(function () {
 });
 
 $('.submit-button').on('click',function(){
-    var a = $('.repeat-div').children().children();
-    var b = Object.entries(a);
-    b.forEach(function(ele){
-        ele.forEach(function(e){
-            console.log(e);
-        })
-    })
+    var cards = $('.repeat-div')
+    // var b = Object.entries(a);
+    console.log(cards);
+    config ={}
+    layers = []
+    for(card of cards){
+        layerconfig = {}
+        let layerType = $(card).children('.card-body').children('.group-input').children('select').val();
+        if(layerType == 'convolution_layer'){
+
+            const data =  $(card).children('.card-body').children('.convolution_layer').children('.group-input');
+            console.log(data[0])
+            const stride = $(data[0]).children('input').val()
+            const filter = $(data[1]).children('input').val();
+            const sameconvolution = $(data[2]).children('input').is(":checked");
+            layerconfig = {layerType,stride,filter,sameconvolution}
+        }
+        else if(layerType == 'dense_layer'){
+            const data =  $(card).children('.card-body').children('.dense_layer').children('.group-input');
+            const numberOfUnits = $(data[0]).children('input').val();
+            console.log(data[1]);
+            const activation = $(data[1]).children('select').val();
+            layerconfig = {layerType,numberOfUnits,activation}
+            
+        }
+        else if(layerType == 'pooling_layer'){
+            const data =  $(card).children('.card-body').children('.pooling_layer').children('.group-input');
+            // console.log($(card).children('.card-body').children('.pooling_layer').children('form-check'));
+            const type =  $(card).children('.card-body').children('.pooling_layer').children('.form-check').children('input[name="radio1"]:checked').val();
+            const filter = $(data[0]).children('input').val();
+            const stride = $(data[1]).children('input').val()
+            layerconfig = {layerType,type,filter,stride}
+        }
+
+        else if(layerType == 'output_layer'){
+            layerconfig = {layerType,softmax:true}
+        }
+
+        layers.push(layerconfig);
+    }
+    config={layers}
+    console.log(config)
 });
